88a07e4122cea30d398a30149cc2cdba3d9d253c
Ibrahim-Soltan <73043087+Ibrahim-Soltan@users.noreply.github.com>
2023-01-03 00:35:19 +0200

    Update app.js

--- app.js
+++ app.js
@@ -245,4 +245,10 @@ app.get('/rome',function (req,res) {console.log(req.session.username)
   }});
 
   
-app.listen(3000);
+const PORT = process.env.PORT || 5000;
+
+// your code
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
